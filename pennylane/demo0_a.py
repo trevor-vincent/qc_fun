929e2a2a732330dbf8f12de3a9e84ec403bec9b5
Trevor Vincent <tvincent@cita.utoronto.ca>
2021-08-09 02:57:34 -0400

    add demo0 pennylane

---
+++ pennylane/demo0_a.py
@@ -0,0 +1,17 @@
+import pennylane as qml
+from pennylane import numpy as np
+
+dev1 = qml.device("default.qubit", wires=1)
+
+def circuit(params):
+    qml.RX(params[0], wires=0)
+    qml.RY(params[1], wires=0)
+    return qml.expval(qml.PauliZ(0))
+
+@qml.qnode(dev1)
+def circuit(params):
+    qml.RX(params[0], wires=0)
+    qml.RY(params[1], wires=0)
+    return qml.expval(qml.PauliZ(0))
+
+print(circuit([0.54, 0.12]))
